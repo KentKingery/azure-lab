68cdbd7f638bbd849d71fbd2aec76f5e68bff06f
Kent Kingery <kent@acmecode.io>
2020-03-27 19:13:51 -0500

    Added AzureLab.ServiceBus

---
+++ AzureLab.ServiceBus/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.ServiceBus;
+
+namespace AzureLab.ServiceBus
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://sb-azurelab.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=3EIXtRHDe9mwnNr0pqxXbqQarfnwYWyy4fis3a8+PxY=";
+        const string QueueName = "user_queue";
+        static IQueueClient queueClient;
+
+        public static async Task Main()
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.WriteLine("=======================");
+            Console.WriteLine("Press ENTER key to exit");
+            Console.WriteLine("=======================");
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
